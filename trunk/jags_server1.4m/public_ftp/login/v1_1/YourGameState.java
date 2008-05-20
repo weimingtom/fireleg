d8449c6867bbb2396daa9f65b9277809f7ee06c0
travis@fireleg.com <travis@fireleg.com@2ba993e0-5443-0410-a0a9-7f1003b63069>
2008-05-20 13:57:42 +0000

    making IGame actually work

---
+++ trunk/jags_server1.4m/public_ftp/login/v1_1/YourGameState.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package public_ftp.login.v1_1;
+
+/**
+ *
+ * @author Compaq_Owner
+ */
+public class YourGameState {
+    public boolean loggedIn = false;
+    public String language = "English";
+    public String from = "";
+}

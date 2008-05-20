afd892d8d88ab82ad1aa7ec6278951ec4c4ce514
travis@fireleg.com <travis@fireleg.com@2ba993e0-5443-0410-a0a9-7f1003b63069>
2008-05-20 13:57:29 +0000

    making IGame actually work

--- trunk/jags_server1.4m/src/jags/multigame/Connection.java
+++ trunk/jags_server1.4m/src/jags/multigame/Connection.java
@@ -6,7 +6,6 @@
  * To change this template, choose Tools | Template Manager
  * and open the template in the editor.
  */
-
 package jags.multigame;
 
 import java.io.BufferedReader;
@@ -20,16 +19,16 @@ import java.net.Socket;
  * @author test
  */
 public class Connection extends Thread {
-    
+
     /** Creates a new instance of Connection */
     public Connection(GameLibrarian librarian, IBroadCast IB, IDie ID, Socket c, int count) {
-        clientSock = c;        
+        clientSock = c;
         this.librarian = librarian;
         this.IB = IB;
         this.ID = ID;
-        this.connectionNumber = count; 
-        this.connectionName = "conn_"+ connectionNumber +"c";
-        Trace.out("CONNECTION STARTED: "+ count +"");        
+        this.connectionNumber = count;
+        this.connectionName = "conn_" + connectionNumber + "c";
+        Trace.out("CONNECTION STARTED: " + count + "");
     }
     
     Socket clientSock;
@@ -39,12 +38,12 @@ public class Connection extends Thread {
     
     // controls killing of idle connections
     IDie ID;
-    public long timeStamp = System.currentTimeMillis(); 
+    public long timeStamp = System.currentTimeMillis();
     public boolean markedToDie = false;
     public long deathTime = timeStamp;
     
-    public static String SECURITY_POLICY = 
-                "<?xml version=\"1.0\"?><cross-domain-policy><allow-access-from domain=\"*\" to-ports=\"80-4000\"/></cross-domain-policy> ";
+    public static String SECURITY_POLICY =
+            "<?xml version=\"1.0\"?><cross-domain-policy><allow-access-from domain=\"*\" to-ports=\"80-4000\"/></cross-domain-policy> ";
     
     
     // turns raw data into readable Actions
@@ -83,11 +82,11 @@ public class Connection extends Thread {
     // Protocol is !more>4 (meaning need 4 more besides this connection)
     // the > is whatever the Message.DELIMITER is.
     int waitingForMore = 0;
-  
+
     public void setGameState(GameState gs) {
         state = gs;
     }
-    
+
     // TODO: use subscriber pattern to allow multiple channels?
     // singular groups for now...
     public void setGroupName(String group) {
@@ -96,112 +95,112 @@ public class Connection extends Thread {
 
     private void setGameName(String gameName) {
         this.gameName = gameName;
-        if (gameTitle != null && gameTitle.getTitle().equals(gameName)) return;
-        
+        if (gameTitle != null && gameTitle.getTitle().equals(gameName)) {
+            return;
+        }
+
         // Remove ourselves from the current GameTitle if exists
         if (gameTitle != null) {
+            gameTitle.unregisterConnection(this);
             gameTitle = null;
         }
-        
+
         // Lookup new title
-        
+
         gameTitle = librarian.lookupCreate(gameName);
-       
+
         // register our connection with that title
         gameTitle.registerConnection(this);
-        
+
         // change our broadcaster to the GameTitle's.
         // now we are in the right game at least.
         this.IB = gameTitle.getIBroadCast();
     }
-    
+
     @Override
     public void run() {
         try {
-            in = new BufferedReader( new InputStreamReader(
-                                                clientSock.getInputStream() ));
-            out = new PrintWriter( clientSock.getOutputStream(), true );
-            sendClient(SECURITY_POLICY +"\u0000");
+            in = new BufferedReader(new InputStreamReader(
+                    clientSock.getInputStream()));
+            out = new PrintWriter(clientSock.getOutputStream(), true);
+            sendClient(SECURITY_POLICY + "\u0000");
             Trace.out("--------------------");
             Trace.out("SECURITY POLICY SENT");
             Trace.out("--------------------");
-            
+
             // the work-horse function
             processClient(in, out);
-            
-            clientSock.close();   
-            Trace.out("CONNECTION CLOSED: "+ connectionName +"\n");            
-        }
-        catch (Exception e) {
-            Trace.out("Run Exception - Connection"+ connectionName +": exception "+ e);
+
+            clientSock.close();
+            Trace.out("CONNECTION CLOSED: " + connectionName + "\n");
+        } catch (Exception e) {
+            Trace.out("Run Exception - Connection" + connectionName + ": exception " + e);
         }
     }
-    
+
     // call this from the parent only (unless you are using this class stand-alone)
     public void kill() {
         try {
+            if (gameTitle != null && gameTitle.IGame != null) {
+                gameTitle.unregisterConnection(this);
+            }            
             //Trace.out("KILL Method "+ connectionNumber);
             this.finalize();
         } catch (Throwable ex) {
             ex.printStackTrace();
-        }        
+        }
     }
-    
+
     // don't worry.  if this raises an exception,
     // the thread probably hasn't finished getting the output object ready.
     public void sendClient(String say) {
         try {
             out.println(say);
             out.flush();
-            Trace.out("OUT: "+ say);
+            Trace.out("OUT: " + say);
             Trace.out("");
         } catch (Exception e) {
-            Trace.out
-                    ("SendClient Method exception: "+ connectionName +" "+ say +":: "+ e);
+            Trace.out("SendClient Method exception: " + connectionName + " " + say + ":: " + e);
         }
     }
-    
+
     public void processClient(BufferedReader in, PrintWriter out) {
         String line;
         boolean done = false;
         try {
             while (!done) {
-                if((line = in.readLine()) == null) {
+                if ((line = in.readLine()) == null) {
                     done = true;
                     Trace.out("NULL input");
-                }
-                else {
+                } else {
                     timeStamp = System.currentTimeMillis();
                     markedToDie = false;
-                    
+
                     // DO Game ACTIONS and Server COMMANDS
                     handleMessage(line.trim());
                 }
             }
+        } catch (IOException e) {
+            Trace.out("Process Client IO Exception " + connectionName + ": " + e.toString());
+        } catch (Exception e2) {
+            Trace.out("Process Client Exception " + connectionName + ": " + e2.toString());
         }
-        catch (IOException e) {
-            Trace.out("Process Client IO Exception "+ connectionName +": "+ e.toString());
-        }
-        catch (Exception e2) {
-            Trace.out("Process Client Exception "+ connectionName +": "+ e2.toString());
-        }
-        
+
         ID.die(this);
     }
-    
+
     private boolean handleMessage(String fullMessage) throws Exception {
         try {
-            Trace.out("IN "+ this.connectionName +": " + fullMessage);            
-            message.parseMessage(fullMessage);
-        }
-        catch (Exception ex) {
+            Trace.out("IN " + this.connectionName + ": " + fullMessage);
+            message.parseMessage(fullMessage, this);
+        } catch (Exception ex) {
             if (Rules.USE_TIMESTAMPS) {
                 // Auto-add a timestamp that client forgot and try again
-                Trace.out("IN "+ this.connectionName +" Added Time: " + fullMessage);
-                message.parseMessage(System.currentTimeMillis() + Message.DELIMITER  
-                                    + fullMessage);
+                Trace.out("IN " + this.connectionName + " Added Time: " + fullMessage);
+                message.parseMessage(System.currentTimeMillis() + Message.DELIMITER + fullMessage, this);
+            } else {
+                throw ex;
             }
-            else throw ex;
         }
 
         // Send to Rules for possible transformation
@@ -209,83 +208,75 @@ public class Connection extends Thread {
         //action = message.action;
 
         //if (!action.isValid) return false;
-        
+
         // send it to the dynamically selected Gamestate
         // this gives your game a chance to alter the message,
         // and correct recipients.
-       
-        if (gameTitle != null && gameTitle.IGame != null) gameTitle.IGame.doAction(message);
-        
+        if (gameTitle != null && gameTitle.IGame != null) {
+            gameTitle.IGame.doAction(message);
+        }
+
         //Trace.out(message.toString());
         if (message.tellServer) {
             return doServerCommand(message);
-        }
-        else {
+        } else {
             // Broadcast to correct recipients.
-            IB.broadcast(connectionNumber, message); 
-            return true;            
+            IB.broadcast(connectionNumber, message);
+            return true;
         }
     }
-    
+
     private boolean doServerCommand(Message comm) {
         String line = message.action.verb;
         boolean done = false;
 
-        
+
         // Now that the client connection has "warmed up",
         // we can kick off a request to nail down its game name asap.
-        if (gameName == null) sendClient("!what_is_your_game");
-        
+        if (gameName == null) {
+            sendClient("!what_is_your_game");
+        }
+
         // PROTOCOL COMMANDS
         if (line.trim().indexOf("!bye") > -1) {
             done = true;
 
-            // still need to tell rules that we're leaving,
-            // so other players see us gone or whatever the rule is.
-            // TODO: possibly move this to ACTIONS section
-            // or broadcast the player leaving.
-        }
-        // keep your connection from being killed
+        // still need to tell rules that we're leaving,
+        // so other players see us gone or whatever the rule is.
+        // TODO: possibly move this to ACTIONS section
+        // or broadcast the player leaving.
+        } // keep your connection from being killed
         else if (line.trim().indexOf("!iam_alive") > -1) {
-            Trace.out(connectionName +" is alive.");
-        }
-        else if (line.trim().indexOf("!whats_my_name") > -1) {
-            sendClient("!your_id" + Message.CLIENT_DELIMITER + this.connectionName); 
-        }
-        else if (line.trim().indexOf("!whats_my_game") > -1) {
-            sendClient("!your_game" + Message.CLIENT_DELIMITER + this.gameName); 
-        }
-        else if (line.trim().indexOf("!set_my_group") > -1) {
+            Trace.out(connectionName + " is alive.");
+        } else if (line.trim().indexOf("!whats_my_name") > -1) {
+            sendClient("!your_id" + Message.CLIENT_DELIMITER + this.connectionName);
+        } else if (line.trim().indexOf("!whats_my_game") > -1) {
+            sendClient("!your_game" + Message.CLIENT_DELIMITER + this.gameName);
+        } else if (line.trim().indexOf("!set_my_group") > -1) {
             String[] data = line.trim().split(Message.CLIENT_DELIMITER);
             this.setGroupName(data[1]);
-        }        
-        else if (line.trim().indexOf("!set_my_game") > -1) {
+        } else if (line.trim().indexOf("!set_my_game") > -1) {
             String[] data = line.trim().split(Message.CLIENT_DELIMITER);
             this.setGameName(data[1]);
-        }        
-        // automatically find more players
+        } // automatically find more players
         else if (line.trim().indexOf("!auto_match") > -1) {
             String[] data = line.trim().split(Message.CLIENT_DELIMITER);
             Trace.out(
-                    groupName +":"+ connectionName 
-                    +" looking for up to "+ data[1] +" total players.");
+                    groupName + ":" + connectionName + " looking for up to " + data[1] + " total players.");
 
             waitingForMore = Integer.parseInt(data[1]);
 
             IB.autoMatch(this);
-        }
-        else if (line.trim().indexOf("!start_game") > -1) {
+        } else if (line.trim().indexOf("!start_game") > -1) {
             // remove game from autoMatch
             // broadcast to group that game started
             IB.gameStarted(this);
-            IB.removeHost(this);                            
-        }                    
-        else {
-            Trace.out("COMMAND not found."+ line);
+            IB.removeHost(this);
+        } else {
+            Trace.out("COMMAND not found." + line);
             Trace.out("");
         }
-        
+
         return done;
     }
-    
 }
